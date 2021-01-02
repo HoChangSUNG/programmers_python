278e14de9c4fb3f483055ee198690f8fa5099909
HoChangSUNG <love47024702@naver.com>
2021-01-02 16:15:56 +0900

    programmers_level_2_가장큰수

---
+++ level_2/most_big_number.py
@@ -0,0 +1,11 @@
+def solution(numbers):
+    numbers=list(map(str,numbers))
+    sorted_numbers= sorted(numbers,key = lambda num : num*3,reverse=True)
+    sum = 0
+    for num in list(map(int,sorted_numbers)) :
+        sum += num
+    if sum ==0 :
+        return '0'
+    return ''.join(sorted_numbers)
+numbers = [3, 30, 34, 5, 9]
+print(solution(numbers))
